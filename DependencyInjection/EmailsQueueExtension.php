eae26c20a760e3e04e7075089b86b4fb4532a0b2
Julien G <julien@Juliens-MacBook-Pro.local>
2018-10-17 20:31:44 +0800

    dep inject

---
+++ DependencyInjection/EmailsQueueExtension.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace JulienIts\EmailsQueueBundle\Controller\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
+
+class EmailsQueueExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        die('load');
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+        $loader->load('services.yml');
+    }
+}
